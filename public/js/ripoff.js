bb7cf9c5b563150a8a63c8316c02f2e5bbb834dd
Stuart Nelson <stuartnelson3@gmail.com>
2014-02-05 21:30:05 -0500

    colorful d3 sphere visualizer!

--- public/js/ripoff.js
+++ public/js/ripoff.js
@@ -9,12 +9,9 @@ function loadSound(url) {
   request.open('GET', url, true);
   request.responseType = 'arraybuffer';
 
-  // When loaded decode the data
   request.onload = function() {
 
-    // decode the data
     context.decodeAudioData(request.response, function(buffer) {
-      // when the audio is decoded play the sound
       playSound(buffer);
     }, onError);
   }
@@ -27,19 +24,19 @@ function playSound(buffer) {
   sourceNode.start(0);
 }
 
-// log if an error occurs
 function onError(e) {
   console.log(e);
 }
 
 function drawSpectrum(array) {
   // do the d3 here
-  for ( var i = 0; i < (array.length); i++ ){
-    var value = array[i];
-
-    ctx.fillRect(i*5,325-value,3,325);
-    //  console.log([i,value])
-  }
+  var wiggle = function(start) {
+    return function(d,i) { return start + (array[i]*0.08)|0; };
+  };
+  circles.transition().duration(0)
+  .style('fill', function(d,i) { return color(array[i]); })
+  .attr('r', wiggle(5))
+  // .attr('cy', wiggle(60))
 }
 
 $(function() {
@@ -51,39 +48,33 @@ $(function() {
   }
   context = new AudioContext();
 
-  // get the context from the canvas to draw on
+  svg = d3.select('.container').append('svg').attr('height', 420).attr('width', 960);
+  var data = new Array();
+  for (var i = 0; i < 32; data.push(i++)) {}
+  color = d3.scale.category10();
+  circles = svg.selectAll('circle').data(data);
+  circles.enter().append('circle')
+  .style('fill', 'steelblue')
+  .transition()
+  .duration(750)
+  .attr('cy', 60)
+  .attr('cx', function(d,i) { return i*30 + 20; })
+  .attr('r', function(d,i) { return 5; })
+  .style('fill', function(d,i) { return color(i); })
 
-  var canvas = document.createElement("canvas")
-  document.querySelector(".container").appendChild(canvas);
-  ctx = document.querySelector("canvas").getContext("2d");
 
-  // create a gradient for the fill. Note the strange
-  // offset, since the gradient is calculated based on
-  // the canvas, not the specific element we draw
-  var gradient = ctx.createLinearGradient(0,0,0,300);
-  gradient.addColorStop(1,'#000000');
-  gradient.addColorStop(0.75,'#ff0000');
-  gradient.addColorStop(0.25,'#ffff00');
-  gradient.addColorStop(0,'#ffffff');
-
-
-  // load the sound
   setupAudioNodes();
 
   function setupAudioNodes() {
 
-    // setup a javascript node
     javascriptNode = context.createScriptProcessor(2048, 1, 1);
-    // connect to destination, else it isn't called
     javascriptNode.connect(context.destination);
 
 
-    // setup a analyzer
     analyser = context.createAnalyser();
     analyser.smoothingTimeConstant = 0.3;
-    analyser.fftSize = 512;
+    analyser.fftSize = 32;
 
-    // create a buffer source node
     sourceNode = context.createBufferSource();
     sourceNode.connect(analyser);
     analyser.connect(javascriptNode);
@@ -91,23 +82,11 @@ $(function() {
     sourceNode.connect(context.destination);
   }
 
-  // load the specified sound
-  // when the javascript node is called
-  // we use information from the analyzer node
-  // to draw the volume
   javascriptNode.onaudioprocess = function() {
 
-    // get the average for the first channel
     var array =  new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(array);
-
-    // clear the current state
-    ctx.clearRect(0, 0, 1000, 325);
-
-    // set the fill style
-    ctx.fillStyle=gradient;
     drawSpectrum(array);
-
-  }
+  };
 
 });
