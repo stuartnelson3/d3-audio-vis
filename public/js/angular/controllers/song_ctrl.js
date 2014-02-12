f7e58319e746a38c14102ffd057485d237f50a0d
Stuart Nelson <stuartnelson3@gmail.com>
2014-02-11 20:36:54 -0500

    move more ctrl code into audio_player

--- public/js/angular/controllers/song_ctrl.js
+++ public/js/angular/controllers/song_ctrl.js
@@ -2,22 +2,13 @@ angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", "AudioPl
   // fun to mess with playbackRate
   // sourceNode.playbackRate.value
   var audioPlayer = new AudioPlayer();
+  audioPlayer.setupAudioNodes();
   $scope.context = audioPlayer.audioContext;
+  $scope.analyser = audioPlayer.analyser;
+  $scope.javascriptNode = audioPlayer.javascriptNode;
 
   $scope.data = [];
   for (var i = 0; i < 32; $scope.data.push(i++)) {}
-
-  setupAudioNodes();
-
-  function setupAudioNodes() {
-    $scope.analyser = $scope.context.createAnalyser();
-    $scope.analyser.smoothingTimeConstant = 0.3;
-    $scope.analyser.fftSize = 32;
-
-    $scope.javascriptNode = $scope.context.createScriptProcessor(2048, 1, 1);
-    $scope.javascriptNode.connect($scope.context.destination);
-    $scope.analyser.connect($scope.javascriptNode);
-  }
 
   $scope.loadSound = function(url) {
     // need to remove or stop old element before playing new one
--- public/js/angular/resources/audio_player.js
+++ public/js/angular/resources/audio_player.js
@@ -1,4 +1,5 @@
 angular.module("SongVis.resources").factory("AudioPlayer", [function() {
+
   var audioPlayer = function() {
     if (!window.AudioContext) {
       if (!window.webkitAudioContext) {
@@ -8,6 +9,17 @@ angular.module("SongVis.resources").factory("AudioPlayer", [function() {
     }
     this.audioContext = new AudioContext();
   };
+
+  audioPlayer.prototype.setupAudioNodes = function() {
+    this.analyser = this.audioContext.createAnalyser();
+    this.analyser.smoothingTimeConstant = 0.3;
+    this.analyser.fftSize = 32;
+
+    this.javascriptNode = this.audioContext.createScriptProcessor(2048, 1, 1);
+    this.javascriptNode.connect(this.audioContext.destination);
+    this.analyser.connect(this.javascriptNode);
+  }
+
 
   return audioPlayer;
 }]);
