605933715a13b7461317ad1e3fb2bc40ea424044
Stuart Nelson <stuartnelson3@gmail.com>
2014-02-11 20:05:51 -0500

    remove globals from song ctrl, put vars on $scope

--- public/js/angular/controllers/song_ctrl.js
+++ public/js/angular/controllers/song_ctrl.js
@@ -1,18 +1,13 @@
 angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", function($scope) {
   // fun to mess with playbackRate
   // sourceNode.playbackRate.value
-  var audioBuffer;
-  var sourceNode;
-  var analyser;
-  var javascriptNode;
-
   if (! window.AudioContext) {
     if (! window.webkitAudioContext) {
       alert('no audiocontext found');
     }
     window.AudioContext = window.webkitAudioContext;
   }
-  context = new AudioContext();
+  $scope.context = new AudioContext();
 
   $scope.data = new Array();
   for (var i = 0; i < 32; $scope.data.push(i++)) {}
@@ -20,30 +15,29 @@ angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", function
   setupAudioNodes();
 
   function setupAudioNodes() {
-    context = new webkitAudioContext();
-    analyser = context.createAnalyser();
-    analyser.smoothingTimeConstant = 0.3;
-    analyser.fftSize = 32;
+    $scope.context = new webkitAudioContext();
+    $scope.analyser = $scope.context.createAnalyser();
+    $scope.analyser.smoothingTimeConstant = 0.3;
+    $scope.analyser.fftSize = 32;
 
-    javascriptNode = context.createScriptProcessor(2048, 1, 1);
-    javascriptNode.connect(context.destination);
-    analyser.connect(javascriptNode);
+    $scope.javascriptNode = $scope.context.createScriptProcessor(2048, 1, 1);
+    $scope.javascriptNode.connect($scope.context.destination);
+    $scope.analyser.connect($scope.javascriptNode);
   }
 
   $scope.loadSound = function(url) {
     // need to remove or stop old element before playing new one
-    audio = new Audio();
-    audio.src = url;
-    source = context.createMediaElementSource(audio);
-    source.connect(analyser);
-    analyser.connect(context.destination);
+    $scope.audio = new Audio(url);
+    var source = $scope.context.createMediaElementSource($scope.audio);
+    source.connect($scope.analyser);
+    $scope.analyser.connect($scope.context.destination);
 
     // audio.controls = true;
-    audio.autoplay = true;
-    javascriptNode.onaudioprocess = function() {
+    $scope.audio.autoplay = true;
+    $scope.javascriptNode.onaudioprocess = function() {
       $scope.$apply(function() {
-        $scope.array = new Uint8Array(analyser.frequencyBinCount);
-        analyser.getByteFrequencyData($scope.array);
+        $scope.array = new Uint8Array($scope.analyser.frequencyBinCount);
+        $scope.analyser.getByteFrequencyData($scope.array);
       });
     };
 
