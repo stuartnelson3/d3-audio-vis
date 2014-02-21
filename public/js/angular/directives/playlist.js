350be7ad86b8ba1ea565e647ea4ba642deb7699c
Stuart Nelson <stuartnelson3@gmail.com>
2014-02-20 19:20:44 -0500

    fix error when playing sequential song, add pause

--- public/js/angular/directives/playlist.js
+++ public/js/angular/directives/playlist.js
@@ -7,24 +7,31 @@ angular.module('SongVis.directives').directive('playlist', ["AudioPlayer", funct
     restrict: 'E',
     templateUrl: 'templates/playlist.html',
     link: function(scope, element, attrs) {
+      scope.audio = new Audio();
+
       var audioPlayer = new AudioPlayer();
       audioPlayer.setupAudioNodes();
       scope.context = audioPlayer.audioContext;
       scope.analyser = audioPlayer.analyser;
       scope.javascriptNode = audioPlayer.javascriptNode;
 
+      var source = scope.context.createMediaElementSource(scope.audio);
+      source.connect(scope.analyser);
+      scope.analyser.connect(scope.context.destination);
+
       scope.remove = function(index) {
         scope.songs.splice(index, 1);
+        scope.audio.pause();
+      };
+
+      scope.playing = function() {
+        !scope.audio.paused
       };
 
       scope.play = function(url) {
         // need to remove or stop old element before playing new one
-        scope.audio = new Audio(url);
-        var source = scope.context.createMediaElementSource(scope.audio);
-        source.connect(scope.analyser);
-        scope.analyser.connect(scope.context.destination);
+        scope.audio.src = url;
 
-        // audio.controls = true;
         scope.audio.autoplay = true;
         scope.javascriptNode.onaudioprocess = function() {
           $scope.$apply(function() {
