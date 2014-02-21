88a3fb7012d3555d514d5fdd968584ecef65352e
Stuart Nelson <stuartnelson3@gmail.com>
2014-02-21 18:52:55 -0500

    add tab controls for showing search, url, and vis windows

--- public/js/angular/controllers/song_ctrl.js
+++ public/js/angular/controllers/song_ctrl.js
@@ -1,12 +1,13 @@
 angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", function($scope) {
   $scope.songs = songs;
-
+  $scope.selectedSongs = [];
+  $scope.showTab = 'search';
   $scope.data = [];
   for (var i = 0; i < 32; $scope.data.push(i++)) {}
 
   $scope.update = function(d3Selection) {
     return function(array) {
-      if (!array) return;
+      if (!array || $scope.showTab !== 'vis') return;
       var wiggle = function(initial) {
         return function(d,i) {
           var amnt = (array[i]*0.2)|0
@@ -26,10 +27,12 @@ angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", function
     };
   }
 
-  $scope.selectedSongs = [];
-
   $scope.queue = function(song) {
     $scope.selectedSongs.push(song);
+  };
+
+  $scope.toggleTab = function(tab) {
+    $scope.showTab = tab;
   };
 
 }]);
