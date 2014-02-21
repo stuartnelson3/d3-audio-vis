2fb1b04bdb61d3d2ec18a45b64afe1c7ca043930
Stuart Nelson <stuartnelson3@gmail.com>
2014-02-20 19:06:47 -0500

    play songs from playlist

--- public/js/angular/controllers/song_ctrl.js
+++ public/js/angular/controllers/song_ctrl.js
@@ -1,11 +1,4 @@
-angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", "AudioPlayer", function($scope, AudioPlayer) {
-  // fun to mess with playbackRate
-  // sourceNode.playbackRate.value
-  var audioPlayer = new AudioPlayer();
-  audioPlayer.setupAudioNodes();
-  $scope.context = audioPlayer.audioContext;
-  $scope.analyser = audioPlayer.analyser;
-  $scope.javascriptNode = audioPlayer.javascriptNode;
+angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", function($scope) {
   $scope.songs = songs;
 
   $scope.data = [];
@@ -36,24 +29,6 @@ angular.module("SongVis.controllers").controller("SongCtrl", ["$scope", "AudioPl
 
   $scope.queue = function(song) {
     $scope.selectedSongs.push(song);
-  };
-
-  $scope.loadSound = function(url) {
-    // need to remove or stop old element before playing new one
-    $scope.audio = new Audio(url);
-    var source = $scope.context.createMediaElementSource($scope.audio);
-    source.connect($scope.analyser);
-    $scope.analyser.connect($scope.context.destination);
-
-    // audio.controls = true;
-    $scope.audio.autoplay = true;
-    $scope.javascriptNode.onaudioprocess = function() {
-      $scope.$apply(function() {
-        $scope.array = new Uint8Array($scope.analyser.frequencyBinCount);
-        $scope.analyser.getByteFrequencyData($scope.array);
-      });
-    };
-
   };
 
 }]);
--- public/js/angular/directives/playlist.js
+++ public/js/angular/directives/playlist.js
@@ -1,14 +1,40 @@
-angular.module('SongVis.directives').directive('playlist', [function() {
+angular.module('SongVis.directives').directive('playlist', ["AudioPlayer", function(AudioPlayer) {
   return {
     scope: {
-      songs: '='
+      songs: '=',
+      array: '='
     },
     restrict: 'E',
     templateUrl: 'templates/playlist.html',
     link: function(scope, element, attrs) {
+      var audioPlayer = new AudioPlayer();
+      audioPlayer.setupAudioNodes();
+      scope.context = audioPlayer.audioContext;
+      scope.analyser = audioPlayer.analyser;
+      scope.javascriptNode = audioPlayer.javascriptNode;
+
       scope.remove = function(index) {
         scope.songs.splice(index, 1);
       };
+
+      scope.play = function(url) {
+        // need to remove or stop old element before playing new one
+        scope.audio = new Audio(url);
+        var source = scope.context.createMediaElementSource(scope.audio);
+        source.connect(scope.analyser);
+        scope.analyser.connect(scope.context.destination);
+
+        // audio.controls = true;
+        scope.audio.autoplay = true;
+        scope.javascriptNode.onaudioprocess = function() {
+          $scope.$apply(function() {
+            scope.array = new Uint8Array(scope.analyser.frequencyBinCount);
+            scope.analyser.getByteFrequencyData(scope.array);
+          });
+        };
+
+      };
+
     }
   };
 }]);
